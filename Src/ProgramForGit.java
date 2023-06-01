e22aba5780ed6d2b0ab8f817adb37cd03236040b
AndreyKhrypach <pacik78@gmail.com>
2023-06-01 16:42:42 +0300

    second message hello

--- Src/ProgramForGit.java
+++ Src/ProgramForGit.java
@@ -1,5 +1,6 @@
 public class ProgramForGit {
     public static void main(String[] args) {
         System.out.println("Hello git from Inteleij");
+        System.out.println("Again Hello git from Inteleij");
     }
 }
